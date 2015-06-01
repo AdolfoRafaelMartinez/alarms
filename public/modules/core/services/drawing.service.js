bbd88203296bcaa342925e80810d1ebd368e2932
Marian Vasile <marian.vasile@gmail.com>
2015-05-31 19:10:13 -0500

    added translucency and strokeline

--- public/modules/core/services/drawing.service.js
+++ public/modules/core/services/drawing.service.js
@@ -17,6 +17,13 @@ angular.module('core').service('Drawing', [
         var radius;
         var ap_index = 0;
 
+        var AP_CIRCLE_STROKE_RGB = '#888';
+        var AP_CIRCLE_RGBA = 'rgba(180, 220, 255, 0.5)';
+        var AP_CIRCLE_RGBA_OPAQUE = 'rgba(200, 200, 255, 1)';
+        var DISTANCE_STROKE_RGB = '#ccc';
+        var DISTANCE_TEXT_RGB = '#888';
+        var DISTANCE_CUT_OFF = 80;
+
         var tick = function(event) {
             // this set makes it so the stage only re-renders when an event handler indicates a change has happened.
             if (update) {
@@ -55,28 +62,33 @@ angular.module('core').service('Drawing', [
                         names[i].p_end.x = ap.x;
                         names[i].p_end.y = ap.y;
                     }
-                    names[i].graphics.clear().setStrokeStyle(1).beginStroke('#ccc')
-                        .moveTo(names[i].p_start.x, names[i].p_start.y)
-                        .lineTo(names[i].p_end.x, names[i].p_end.y);
                     names[i].pdistance  = Math.sqrt(
                                             Math.pow(names[i].ap_start.px - names[i].ap_end.px, 2) +
                                             Math.pow(names[i].ap_start.py - names[i].ap_end.py, 2)
                     );
-                    names[i].ptext.text = names[i].pdistance.toFixed(2) + ' ft';
-                    names[i].ptext.x    = names[i].p_start.x + (names[i].p_end.x - names[i].p_start.x) /2;
-                    names[i].ptext.y    = names[i].p_start.y + (names[i].p_end.y - names[i].p_start.y) /2;
+                    if (names[i].pdistance < DISTANCE_CUT_OFF) {
+                        names[i].graphics.clear().setStrokeStyle(1).beginStroke(DISTANCE_STROKE_RGB)
+                            .moveTo(names[i].p_start.x, names[i].p_start.y)
+                            .lineTo(names[i].p_end.x, names[i].p_end.y);
+                        names[i].ptext.text = names[i].pdistance.toFixed(2) + ' ft';
+                        names[i].ptext.x    = names[i].p_start.x + (names[i].p_end.x - names[i].p_start.x) /2;
+                        names[i].ptext.y    = names[i].p_start.y + (names[i].p_end.y - names[i].p_start.y) /2;
+                    } else {
+                        names[i].graphics.clear();
+                        names[i].ptext.text = '';
+                    }
                 }
                 is_dragging = true;
                 update = true;
             });
 
             ap.on('rollover', function(evt) {
-                this.children[0].graphics.clear().beginFill('#cdf').drawCircle(0, 0, radius);
+                this.children[0].graphics.clear().setStrokeStyle(1).beginFill(AP_CIRCLE_RGBA_OPAQUE).beginStroke(AP_CIRCLE_STROKE_RGB).drawCircle(0, 0, radius);
                 update = true;
             });
 
             ap.on('rollout', function(evt) {
-                this.children[0].graphics.clear().beginFill('DeepSkyBlue').drawCircle(0, 0, radius);
+                this.children[0].graphics.clear().setStrokeStyle(1).beginFill(AP_CIRCLE_RGBA).beginStroke(AP_CIRCLE_STROKE_RGB).drawCircle(0, 0, radius);
                 update = true;
             });
         };
@@ -93,7 +105,9 @@ angular.module('core').service('Drawing', [
                     if (ap != ap2) {
                         m = new createjs.Shape();
                         d = Math.sqrt(Math.pow(ap2.px - ap.px, 2) + Math.pow(ap2.py - ap.py, 2));
-                        m.graphics.setStrokeStyle(1).beginStroke('#ccc').moveTo(ap.x, ap.y).lineTo(ap2.x, ap2.y);
+                        if (d < DISTANCE_CUT_OFF) {
+                            m.graphics.setStrokeStyle(1).beginStroke(DISTANCE_STROKE_RGB).moveTo(ap.x, ap.y).lineTo(ap2.x, ap2.y);
+                        }
                         m.name = getNewName();
                         m.p_start   = {x: ap.x, y: ap.y};
                         m.ap_start  = ap;
@@ -105,6 +119,9 @@ angular.module('core').service('Drawing', [
                         distances.addChild(m);
 
                         var text = new createjs.Text(d.toFixed(2) + ' ft', "12px Arial", "#888888");
+                        if (d > DISTANCE_CUT_OFF) {
+                            text.text = '';
+                        }
                         text.x = m.p_start.x + (m.p_end.x - m.p_start.x) /2;
                         text.y = m.p_start.y + (m.p_end.y - m.p_start.y) /2;
                         text.textBaseline = "alphabetic";
@@ -171,7 +188,7 @@ angular.module('core').service('Drawing', [
             container.py = mperpx * container.y;
 
             circle.id = _.uniqueId();
-            circle.graphics.beginFill('DeepSkyBlue').drawCircle(0, 0, radius);
+            circle.graphics.setStrokeStyle(1).beginFill(AP_CIRCLE_RGBA).beginStroke(AP_CIRCLE_STROKE_RGB).drawCircle(0, 0, radius);
             circle.puddleShape = 'signal';
             circle.regX = circle.regY = 0;
             circle.scaleX = circle.scaleY = circle.scale = 1;
@@ -185,7 +202,7 @@ angular.module('core').service('Drawing', [
             ap.cursor = 'pointer';
             container.addChild(ap);
 
-            var text = new createjs.Text('AP ' + ap_index++, "14px Arial", "#000000");
+            var text = new createjs.Text('AP ' + ap_index++, "14px Arial", DISTANCE_TEXT_RGB);
             text.x = -15;
             text.y = -10;
             text.textBaseline = "alphabetic";
@@ -204,12 +221,12 @@ angular.module('core').service('Drawing', [
                     if (child.puddle_type == 'ap') {
                         for (var i=0; i<child.children.length; i++) {
                             if (child.children[i].puddleShape == 'signal') {
-                                child.children[i].graphics.clear().beginFill('DeepSkyBlue').drawCircle(0, 0, radius);
+                                child.children[i].graphics.clear().setStrokeStyle(1).beginFill(AP_CIRCLE_RGBA).beginStroke(AP_CIRCLE_STROKE_RGB).drawCircle(0, 0, radius);
                             }
                         }
                     }
                 });
-                
+
                 update = true;
             }
         };
