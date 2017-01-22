5b689f328d22a3ae5a8a25eb2757cf04e682bd92
Marian Vasile <marian.vasile@gmail.com>
2017-01-22 08:50:05 -0600

    fixed production

--- config/env/production.js
+++ config/env/production.js
@@ -3,6 +3,40 @@
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/puddlejump',
 	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.css',
+				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/angular-material/angular-material.css',
+				'public/lib/jQueryFileUpload/css/style.css',
+				'public/lib/blueimp-file-upload/css/jquery.fileupload.css',
+				'public/lib/blueimp-file-upload/css/jquery.fileupload-ui.css',
+			],
+			js: [
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.js',
+				'public/lib/angular-cookies/angular-cookies.js',
+				'public/lib/angular-animate/angular-animate.js',
+				'public/lib/angular-touch/angular-touch.js',
+				'public/lib/angular-aria/angular-aria.js',
+				'public/lib/angular-material/angular-material.js',
+				'public/lib/angular-sanitize/angular-sanitize.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+                'public/lib/easeljs/lib/easeljs-NEXT.combined.js',
+                'public/lib/caman/dist/caman.full.min.js',
+
+				'public/lib/blueimp-file-upload/js/vendor/jquery.ui.widget.js',
+                'public/lib/blueimp-load-image/js/load-image.all.min.js',
+                'public/lib/blueimp-canvas-to-blob/js/canvas-to-blob.min.js',
+                'public/lib/blueimp-file-upload/js/jquery.fileupload.js',
+                'public/lib/blueimp-file-upload/js/jquery.fileupload-process.js',
+                'public/lib/blueimp-file-upload/js/jquery.fileupload-image.js',
+                'public/lib/blueimp-file-upload/js/jquery.fileupload-validate.js',
+                'public/lib/blueimp-file-upload/js/jquery.fileupload-angular.js'
+			]
+		},
 		css: 'public/dist/application.min.css',
 		js: 'public/dist/application.min.js'
 	},
